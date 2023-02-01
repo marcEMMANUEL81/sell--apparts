ca2b2f183efd77dbe03e3011bbd3e7f0a06ae724
Brou__Yao <marcemmanuel81@gmail.com>
2023-02-01 12:02:08 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <HomeView />
 </template>
 
+<script>
+import HomeView from './views/HomeView.vue';
+
+export default {
+  components: {
+    HomeView,
+  }
+}
+</script>
+
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,16 +22,36 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+// @import url(‘https://fonts.googleapis.com/css?family=Poppins’);
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+// body{
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     gap: 20px;
+//     align-items: center;
+//     font-family: 'Poppins';
+// }
+
+html
+{
+    scroll-behavior: smooth;
+}
+
+::-webkit-scrollbar
+{
+    background-color: rgb(214, 214, 213);
+    width: 10px;
+}
+
+::-webkit-scrollbar-thumb
+{
+    background-color: goldenrod;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,104 @@
+<template>
+  <header>
+        <div class="nav__container">
+            <div class="nav__logo">BAT <span>CI.</span> </div>
+            <div class="nav__item">
+                <nav>
+                    <a class="item__nav" href="#">Accueil</a>
+                    <a class="item__nav" href="#nos__cartes">Qui sommes nous ?</a>
+                    <a class="item__nav" href="#shop">Ajouter</a>
+                    <a class="item__nav" href="#footer">A propos</a>
+                </nav>
+            </div>  
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+/*----------< Barre de navigation >------------*/
+
+header{
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 999;
+    background-color: #111;
+    padding: 30px 5px;
+}
+
+.nav__container{
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    max-width: 60vw;
+}
+
+.nav__logo{
+    font-size: 20px;
+    font-weight: 600;
+    color: white;
+}
+
+.nav__logo span{
+    color: crimson;
+    margin-left: 5px;
+}
+
+.item__nav{
+    position: relative;
+    text-decoration: none;
+    font-size: 14px;
+    font-weight: 600;
+    color: white;
+    margin-left: 50px;
+    cursor: pointer;
+    transition: ease-in-out .3s;
+}
+
+.item__nav:hover{
+    color: crimson !important;
+}
+
+.item__nav::before{
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: -7px;
+    background-color: crimson;
+    height: 4px;
+    border-radius: 4px;
+    transition: ease-in-out .3s;
+    width: 0%;
+}
+
+.item__nav:hover::before{
+    width: 100%;
+}
+
+/*----------< Barre de navigation >------------*/
+</style>
